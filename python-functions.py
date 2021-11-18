fb49304fd347e55a6e78f0ad765fae013bf6c2ea
Abdul2067 <abdul2067@gmail.com>
2021-11-17 20:11:02 -0500

    solve 3

--- python-functions.py
+++ python-functions.py
@@ -20,3 +20,9 @@ print(largest([4, 46, 56, 454, 45, 86, 76]))
 
 
 # 3. Write a function named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
+
+def occurances(str1, str2):
+  return str1.count(str2)
+print(occurances("waves", "e"))
+
+
