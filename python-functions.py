a202555f09f91bd8c55a377e2080cc2c293cf43a
Abdul2067 <abdul2067@gmail.com>
2021-11-17 21:14:53 -0500

    solve 6

--- python-functions.py
+++ python-functions.py
@@ -26,3 +26,11 @@ def occurances(str1, str2):
 print(occurances("waves", "e"))
 
 
+# 4. Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product. HINT: Review your notes on args.
+
+def product(*args):
+  product = 1
+  for arg in args:
+    product *= arg
+  return product
+print(product(3, 5, 2))
